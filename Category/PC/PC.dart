eafbbbbb89ca0e667285e1ae814fcd2dd83a0f25
SergeyKhmelik <sergey.khmelik@gmail.com>
2020-06-08 03:42:52 +0300

    +norm database -norm read

--- Category/PC/PC.dart
+++ Category/PC/PC.dart
@@ -3,7 +3,10 @@ class PC {
   String _name;
   String _type;
   String _description;
-  PC(this._id, this._type, this._name, this._description);
+  PC(this._id,
+this._type,
+this._name,
+this._description);
   void writeDiscription() {
     print('$_type - $_name, описание:');
     print(_description);
--- Category/PhoneAccessory/PhoneAccessory.dart
+++ Category/PhoneAccessory/PhoneAccessory.dart
@@ -3,7 +3,10 @@ class PhoneAccessory {
   String _name;
   String _type;
   String _description;
-  PhoneAccessory(this._id, this._type, this._name, this._description);
+  PhoneAccessory(this._id,
+this._type,
+this._name,
+this._description);
   void writeDiscription() {
     print('$_type - $_name, описание:');
     print(_description);
