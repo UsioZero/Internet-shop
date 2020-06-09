925e82f6ee506948a852c93d465552c0f386114c
SergeyKhmelik <sergey.khmelik@gmail.com>
2020-06-09 21:49:31 +0300

    -id

--- Category/pc/pc.dart
+++ Category/pc/pc.dart
@@ -1,10 +1,8 @@
 class PC {
-  final _id;
   String _name;
   String type;
   String _description;
-  PC(this._id,
-this.type,
+  PC(this.type,
 this._name,
 this._description);
   void writeDiscription() {
--- Category/phone_accessory/phone_accessory.dart
+++ Category/phone_accessory/phone_accessory.dart
@@ -1,10 +1,8 @@
 class PhoneAccessory {
-  final _id;
   String _name;
   String type;
   String _description;
-  PhoneAccessory(this._id,
-this.type,
+  PhoneAccessory(this.type,
 this._name,
 this._description);
   void writeDiscription() {
